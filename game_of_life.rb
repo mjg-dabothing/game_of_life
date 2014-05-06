de7d1b472e395c71585a3e2ae2a621e87427576e
M.J <mj@Mint13.(none)>
2014-05-06 14:54:28 -0700

    Time to learn Ruby a bit I guess

---
+++ game_of_life.rb
@@ -0,0 +1,12 @@
+#
+# Conway's Game of Life implementation.
+# Developed in Linux Mint virtual machine running in VirtualBox.
+#
+
+class GameOfLife
+
+  def initialize()
+  end
+
+  def evolve()
+  end
