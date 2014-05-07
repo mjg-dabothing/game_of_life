318e3f318ef76cc99fb7df457a6b7d43d9141ced
M.J <mj@Mint13.(none)>
2014-05-06 21:52:18 -0700

    I learned how to deep copy in Ruby!

--- game_of_life.rb
+++ game_of_life.rb
@@ -7,7 +7,10 @@
 class GameOfLife
 
     def initialize(dimension = 5)
-        #@state = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+        #@state = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]] #blinker
+        #@state = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]] #block
+        #@state = [[0, 0, 0, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]] #boat
+        #@state = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0]] #glider
         @state = []
         (0...dimension).each do |row_index|
             state[row_index] = []
@@ -19,9 +22,11 @@ class GameOfLife
     end
 
     def evolve()
-        newstate = @state
-        @state.each_with_index do |row, row_index|
-            row.each_with_index do |col, col_index|
+        newstate = Marshal.load(Marshal.dump(@state))
+        #@state.each_with_index do |row, row_index|
+        (0...newstate.size).each do |row_index|
+            #row.each_with_index do |col, col_index|
+            (0...newstate[row_index].size).each do |col_index|
                 neighbor_count = num_neighbors(row_index, col_index)
                 if (neighbor_count < 2 || neighbor_count > 3)
                     newstate[row_index][col_index] = 0
@@ -36,19 +41,26 @@ class GameOfLife
     end
 
     def num_neighbors(row_index, col_index)
-        num_neighbors = 0;
-        (row_index-1..row_index+1).each do |neighbor_row_index|
-            (col_index-1..col_index+1).each do |neighbor_col_index|
-                unless row_index == neighbor_row_index && col_index == neighbor_col_index
+        neighbors = 0;
+        #puts "checking neighbors of space #{row_index}, #{col_index}"
+        ((row_index-1)..(row_index+1)).each do |neighbor_row_index|
+            #puts "checking neighbor row #{neighbor_row_index}"
+            ((col_index-1)..(col_index+1)).each do |neighbor_col_index|
+                #puts "checking neighbor column #{neighbor_col_index}"
+                if  !(row_index == neighbor_row_index && col_index == neighbor_col_index) #don't count self
                     neighbor_row = neighbor_row_index % @state.size
                     neighbor_col = neighbor_col_index % @state[neighbor_row].size
-                    if @state[neighbor_row][neighbor_col] == 1
-                        num_neighbors += 1
-                    end
+                    #puts "checking neighbor space #{neighbor_row}, #{neighbor_col} == #{@state[neighbor_row][neighbor_col]}"
+                    neighbors += @state[neighbor_row][neighbor_col]
+                    #if @state[neighbor_row][neighbor_col] == 1
+                    #    neighbors += 1
+                    #    puts "neighbor found"
+                    #end
                 end
             end
         end
-        return num_neighbors
+        #puts "num_neighbors: #{neighbors}"
+        return neighbors
     end
 
     attr_accessor :state
--- game_of_life_test.rb
+++ game_of_life_test.rb
@@ -8,11 +8,6 @@
 # first generation: apply pattern
 # 
 
-### EXAMPLE ##########################################################################################################
-# WRITE YOUR OWN TESTS, OF COURSE
-# test-driven development is the best, this is just to show you how it should work (if it's not clear from rules)
-# Plus varying parameters on initialization allows you to do cooler things, like play with different sizes, seeds, etc.
-#######################################################################################################################
 
 
 require File.join(File.dirname(__FILE__), 'game_of_life')
@@ -21,33 +16,60 @@ require 'test/unit'
 
 class GameOfLifeTest < Test::Unit::TestCase
 
-  def setup
-    @game = GameOfLife.new(3)
-  end
+    def setup
+        @game3 = GameOfLife.new(3)
+        @game5 = GameOfLife.new(5)
+    end
 
-  def test_should_kill_with_no_neighbours
-    @game.state = [[1,0,0],[0,0,0],[0,0,0]]
-    after = @game.evolve
-    assert_equal after[0][0], 0
-  end
+    def test_should_kill_with_no_neighbours
+        @game3.state = [[1,0,0],[0,0,0],[0,0,0]]
+        after = @game3.evolve
+        assert_equal after[0][0], 0
+    end
 
-  def test_should_kill_with_just_one_neighbour
-    @game.state = [[0,0,0],[1,0,0],[1,0,0]]
-    after = @game.evolve
-    assert_equal after[1][0], 0
-    assert_equal after[2][0], 0
-  end
+    def test_should_kill_with_just_one_neighbour
+        @game3.state = [[0,0,0],[1,0,0],[1,0,0]]
+        after = @game3.evolve
+        assert_equal after[1][0], 0
+        assert_equal after[2][0], 0
+    end
 
-  def test_should_kill_with_more_than_3_neighbours
-    @game.state = [[1,1,1],[1,1,1],[1,1,1]]
-    after = @game.evolve
-    assert_equal after, [[0,0,0],[0,0,0],[0,0,0]]
-  end
+    def test_should_kill_with_more_than_3_neighbours
+        @game3.state = [[1,1,1],[1,1,1],[1,1,1]]
+        after = @game3.evolve
+        assert_equal after, [[0,0,0],[0,0,0],[0,0,0]]
+    end
 
-  def test_should_give_birth_if_3_neighbours
-    @game.state = [[1,0,0],[1,1,0],[0,0,0]]
-    after = @game.evolve
-    assert_equal after, [[1,1,1],[1,1,1],[1,1,1]]
-  end
+    def test_should_give_birth_if_3_neighbours
+        @game3.state = [[1,0,0],[1,1,0],[0,0,0]]
+        after = @game3.evolve
+        assert_equal after, [[1,1,1],[1,1,1],[1,1,1]]
+    end
+
+    def test_should_oscillate_blinker_vertical
+        @game5.state = [[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]] #blinker, vertical state
+        after = @game5.evolve
+        assert_equal after, [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]] #blinker, horizontal state
+    end
+
+    def test_should_oscillate_blinker_horizontal
+        @game5.state = [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]] #blinker, horizontal state
+        after = @game5.evolve
+        assert_equal after, [[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]] #blinker, vertical state
+    end
+
+    def test_should_be_stable_with_2x2_block
+        @game5.state = [[0,0,0,0,0],[0,1,1,0,0],[0,1,1,0,0],[0,0,0,0,0],[0,0,0,0,0]] #block
+        after = @game5.evolve
+        assert_equal after, [[0,0,0,0,0],[0,1,1,0,0],[0,1,1,0,0],[0,0,0,0,0],[0,0,0,0,0]] #block
+    end
+
+    def test_should_be_stable_with_boat
+        @game5.state = [[0,0,0,0,0], [0,1,1,0,0], [0,1,0,1,0], [0,0,1,0,0], [0,0,0,0,0]] #boat
+        after = @game5.evolve
+        assert_equal after, [[0,0,0,0,0], [0,1,1,0,0], [0,1,0,1,0], [0,0,1,0,0], [0,0,0,0,0]] #boat
+    end
+
+
 
 end
