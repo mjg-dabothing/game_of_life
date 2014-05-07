8df63dd70ce37f6f05f0f50bfd3eb586c8ca6ad9
M.J <mj@Mint13.(none)>
2014-05-06 21:59:03 -0700

    removed debug prints and commented-out code

--- game_of_life.rb
+++ game_of_life.rb
@@ -23,10 +23,8 @@ class GameOfLife
 
     def evolve()
         newstate = Marshal.load(Marshal.dump(@state))
-        #@state.each_with_index do |row, row_index|
-        (0...newstate.size).each do |row_index|
-            #row.each_with_index do |col, col_index|
-            (0...newstate[row_index].size).each do |col_index|
+        @state.each_with_index do |row, row_index|
+            row.each_with_index do |col, col_index|
                 neighbor_count = num_neighbors(row_index, col_index)
                 if (neighbor_count < 2 || neighbor_count > 3)
                     newstate[row_index][col_index] = 0
@@ -42,24 +40,15 @@ class GameOfLife
 
     def num_neighbors(row_index, col_index)
         neighbors = 0;
-        #puts "checking neighbors of space #{row_index}, #{col_index}"
         ((row_index-1)..(row_index+1)).each do |neighbor_row_index|
-            #puts "checking neighbor row #{neighbor_row_index}"
             ((col_index-1)..(col_index+1)).each do |neighbor_col_index|
-                #puts "checking neighbor column #{neighbor_col_index}"
                 if  !(row_index == neighbor_row_index && col_index == neighbor_col_index) #don't count self
                     neighbor_row = neighbor_row_index % @state.size
                     neighbor_col = neighbor_col_index % @state[neighbor_row].size
-                    #puts "checking neighbor space #{neighbor_row}, #{neighbor_col} == #{@state[neighbor_row][neighbor_col]}"
                     neighbors += @state[neighbor_row][neighbor_col]
-                    #if @state[neighbor_row][neighbor_col] == 1
-                    #    neighbors += 1
-                    #    puts "neighbor found"
-                    #end
                 end
             end
         end
-        #puts "num_neighbors: #{neighbors}"
         return neighbors
     end
 
