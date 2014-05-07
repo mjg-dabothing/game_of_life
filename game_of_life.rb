c16e3ff330f9e5d45c7b34fe58f59e948c9003fa
M.J <mj@Mint13.(none)>
2014-05-06 18:05:15 -0700

    just enough to get something showing, plus adding a couple tests.

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,12 +1,31 @@
 #
 # Conway's Game of Life implementation.
+# Ruby version 1.8.7
 # Developed in Linux Mint virtual machine running in VirtualBox.
 #
 
 class GameOfLife
 
-  def initialize()
-  end
+    def initialize(dimension = 5)
+        #@state = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+        @state = []
+        (0...dimension).each do |row|
+            state[row] = []
+            (0...dimension).each do |col|
+                space = rand(2) #0 or 1.
+                state[row] << space
+            end
+        end
+    end
 
-  def evolve()
-  end
+    def evolve()
+        @state.each_with_index do |row, row_index|
+            row.each_with_index do |col, col_index|
+            end
+        end
+        return @state
+    end
+
+    attr_accessor :state
+
+end
